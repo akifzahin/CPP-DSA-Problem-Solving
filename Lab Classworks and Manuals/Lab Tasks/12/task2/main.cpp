ad083983317673deb19f9d115451298186a4178c
Akif Zahin <zahinakif25@gmail.com>
2023-04-09 17:01:02 +0600

    Updated files

--- Lab Classworks and Manuals/Lab Tasks/12/task2/main.cpp
+++ Lab Classworks and Manuals/Lab Tasks/12/task2/main.cpp
@@ -1,10 +1,46 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "stack.cpp"
-
 using namespace std;
 
+void StringSpaceRemove(string &input1)
+{
+    input1.erase(remove(input1.begin(), input1.end(), ' '), input1.end());
+
+}
 int main()
 {
-    StackType stack1;
+    StackType<int> stack1;
+
+    string input;
+    cout<<"Enter a string of parentheses: \n";
+    getline(cin,input);
+
+    StringSpaceRemove(input);
+
+    for(int i=0; i<5; i++)
+    {
+        if(input[i]=='(')
+        {
+            stack1.Push(input[i]);
+        }
+        else if(input[i]==')')
+        {
+            stack1.Pop();
+        }
+
+
+    }
+
+    if(stack1.IsEmpty() == 1)
+    {
+        cout<<"Balanced.";
+    }
+    else
+    {
+        cout<<"Not Balanced.";
+    }
+
+
+
 
 }

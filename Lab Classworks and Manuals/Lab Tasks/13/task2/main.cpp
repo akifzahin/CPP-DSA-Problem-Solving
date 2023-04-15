10acb23fa762eade0f063cb65e986162144041e8
Akif Zahin <zahinakif25@gmail.com>
2023-04-16 04:01:48 +0600

    Updated Tasks

---
+++ Lab Classworks and Manuals/Lab Tasks/13/task2/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "quetype.cpp"
+using namespace std;
+void BinaryQueue(int n)
+{
+    QueType<int> queue1;
+    queue1.Enqueue(1);
+
+    int inputNumber,resultNumber;
+    for(int i=0; i<n; i++)
+    {
+      for(int j=i; j<n; j++)
+      {
+          queue1.Dequeue(resultNumber);
+            queue1.Enqueue(resultNumber);
+          cout<<resultNumber;
+
+      }
+
+        queue1.Enqueue(0);
+        queue1.Enqueue(1);
+        cout<<"\n";
+
+
+    }
+}
+int main()
+{
+   int n;
+   cout<<"Enter n: ";
+   cin>>n;
+
+    BinaryQueue(n);
+
+}
